Fix stoch gradient from lab 2

labs/ex02/template/stochastic_gradient_descent.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 """Stochastic Gradient Descent"""
 def compute_stoch_gradient(y, tx, w):
-    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient computation.It's same as the gradient descent.
-    # ***************************************************
-    return compute_gradient(y_s, x_s, w)
-
+    dist = (y - tx @ w.T)
+    dist[dist < 0] = -1
+    dist[dist > 0] = 1
+    return 1/(- 2 * (tx.shape[0])) * np.sum(dist[:, np.newaxis] * tx, axis=0)
 
 def stochastic_gradient_descent(
         y, tx, initial_w, batch_size, max_iters, gamma):
@@ -17,7 +14,7 @@ def stochastic_gradient_descent(
     w = initial_w
     for n_iter in range(max_iters):
         for y_s, x_s in batch_iter(y, tx, batch_size):
-            loss = compute_gradient(y_s, x_s, w)
+            loss = compute_stoch_gradient(y_s, x_s, w)
             w = w - gamma * loss
             # store w and loss
             ws.append(w)